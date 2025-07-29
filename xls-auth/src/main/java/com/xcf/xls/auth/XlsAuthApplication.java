c349eb3fb01e518286ef69093d8fad0c962389ce
big-light <xcf941743128@163.com>
2025-07-30 00:32:06 +0800

    初始化-搭建maven多模块

---
+++ xls-auth/src/main/java/com/xcf/xls/auth/XlsAuthApplication.java
@@ -0,0 +1,13 @@
+package com.xcf.xls.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XlsAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XlsAuthApplication.class, args);
+    }
+
+}
---
+++ xls-auth/src/test/java/com/xcf/xls/auth/XlsAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xcf.xls.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XlsAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
