ee4734533c7bc97205b4b741cde3a5b15305b33f
jaca.xu <jack.xu@tec-do.com>
2025-07-30 15:19:20 +0800

    整合mybatis&druid数据库连接池&mybatis代码生成器&自定义Jackson配置

--- xls-auth/src/main/java/com/xcf/xls/auth/XlsAuthApplication.java
+++ xls-auth/src/main/java/com/xcf/xls/auth/XlsAuthApplication.java
@@ -1,8 +1,9 @@
 package com.xcf.xls.auth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.xcf.xls.auth.domain.mapper")
 @SpringBootApplication
 public class XlsAuthApplication {
 
